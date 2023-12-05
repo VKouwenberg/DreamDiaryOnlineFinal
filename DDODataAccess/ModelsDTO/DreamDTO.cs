d67376d3afc1ea745647975d16e2aef31b83da13
McFinnigan <v.kouwenberg@student.fontys.nl>
2023-12-05 11:33:10 +0100

    Update with main Logic and Dal

--- DDODataAccess/ModelsDTO/DreamDTO.cs
+++ DDODataAccess/ModelsDTO/DreamDTO.cs
@@ -5,15 +5,19 @@ namespace DataAccessDDO.ModelsDTO;
 
 public class DreamDTO
 {
-    [Key]
-    public int DreamId { get; set; } //pk
+	[Key]
+	public int DreamId { get; set; } //pk
 
-    [Required]
-    [MaxLength(255)]
-    public string DreamName { get; set; }
+	[Required]
+	[MaxLength(255)]
+	public string DreamName { get; set; }
 
-    [Required]
-    public string DreamText { get; set; }
-    public int DreamerId { get; set; } //fk
-    public DreamerDTO DreamerDTO { get; set; } //nav
+	[Required]
+	public string DreamText { get; set; }
+
+	[Required]
+	public string ReadableBy { get; set; }
+
+	public int DreamerId { get; set; } //fk
+	public DreamerDTO DreamerDTO { get; set; } //nav
 }
